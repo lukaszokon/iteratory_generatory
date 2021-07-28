52a2693cb1a51e91c2729e45db6661d06479a85c
Łukasz Okoń <l.okon@haso.pl>
2021-07-28 18:17:26 +0200

    iterator examples

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 if __name__ == '__main__':
-    pass
+    a = [1, 2, 3, 4, 5]
+    for liczba in a:
+        print(liczba)
+
+    a = 'Ala ma kota'
+    for litera in a:
+        print(litera)
+
+    a = {'imie': 'Adam', 'nazwisko': 'Kowalski'}
+    for item in a:
+        print(item)
