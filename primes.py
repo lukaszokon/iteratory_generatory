39b53edf29d225e5b944a3dab75e3f9eec17b14c
Łukasz Okoń <l.okon@haso.pl>
2021-07-28 19:17:06 +0200

    add PrimeIterator class with iterator

--- primes.py
+++ primes.py
@@ -1,10 +1,12 @@
 from math import sqrt
 
+
 def is_prime(n):
     for i in range(2, int(sqrt(n)) + 1):
-        if n%i == 0:
+        if n % i == 0:
             return False
     return True
+
 
 def get_n_primes(n):
     primes = []
@@ -14,3 +16,22 @@ def get_n_primes(n):
             primes.append(i)
         i += 1
     return primes
+
+class PrimeIterator:
+
+    def __init__(self, n):
+        self.n = n
+        self.generated_numbers = 0
+        self.number = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.number += 1
+        if self.generated_numbers >= self.n:
+            raise StopIteration
+        elif is_prime(self.number):
+            self.generated_numbers += 1
+            return self.number
+        return self.__next__()
