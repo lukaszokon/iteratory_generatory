822a91d342e5bf96b3b3a307924a8676052f174d
Łukasz Okoń <l.okon@haso.pl>
2021-07-28 19:10:31 +0200

    add prime numbers generator

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import primes
+
 if __name__ == '__main__':
-    a = [1, 2, 3, 4, 5]
-    for index, liczba in enumerate(a):
-        print(f'Index: {index}, Wartość: {liczba}')
+    # a = [1, 2, 3, 4, 5]
+    # for index, liczba in enumerate(a):
+    #     print(f'Index: {index}, Wartość: {liczba}')
+    #
+    # a = 'Ala ma kota'
+    # for litera in a:
+    #     print(litera)
+    #
+    # a = {'imie': 'Adam', 'nazwisko': 'Kowalski', 'wiek': 25}
+    # for item in a:
+    #     print(item)
 
-    a = 'Ala ma kota'
-    for litera in a:
-        print(litera)
-
-    a = {'imie': 'Adam', 'nazwisko': 'Kowalski', 'wiek': 25}
-    for item in a:
-        print(item)
+    lista = primes.get_n_primes(100000)
+    for liczba in lista:
+        print(liczba)
---
+++ primes.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+def is_prime(n):
+    for i in range(2, int(sqrt(n)) + 1):
+        if n%i == 0:
+            return False
+    return True
+
+def get_n_primes(n):
+    primes = []
+    i = 2
+    while len(primes) != n:
+        if is_prime(i):
+            primes.append(i)
+        i += 1
+    return primes
